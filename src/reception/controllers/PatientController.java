b8c566d78de089f0b20ab0c54801f704a80a7749
ParnianGhanbarpour <Parnian.ghn83@gmail.com>
2024-09-21 19:48:05 +0330

    validation and PatientController

--- src/reception/controllers/PatientController.java
+++ src/reception/controllers/PatientController.java
@@ -1,13 +1,68 @@
 package reception.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import reception.model.da.PatientDa;
+import reception.model.entity.Patient;
+import reception.model.utils.Validation;
+
 
 import java.net.URL;
+
 import java.util.ResourceBundle;
 
 public class PatientController implements Initializable {
+    private final Validation validation = new Validation();
+
+    @FXML
+    private TextField usernameTxt,nationalIdTxt,nameTxt,familyTxt,phoneNumberTxt,diseaseTxt;
+    @FXML
+    private Button saveBtn,editBtn,removeBtn;
+    @FXML
+    private PasswordField passwordTxt;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        resetForm();
+        saveBtn.setOnAction(event -> {
+            try (PatientDa patientDa = new PatientDa()) {
+                Patient patient =
+                        Patient
+                                .builder()
+                                .username(usernameTxt.getText())
+                                .password(passwordTxt.getText())
+                                .name(validation.NameValidator(nameTxt.getText()))
+                                .family(validation.familyValidator(familyTxt.getText()))
+                                .disease(validation.diseaseValidator(diseaseTxt.getText()))
+                                .nationalId(validation.nationalIdValidator(nationalIdTxt.getText()))
+                                .phoneNumber(validation.phoneNumberValidator(phoneNumberTxt.getText()))
+                                .build();
+                patientDa.save(patient);
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION, "Person Saved\n" + patient);
+                alert.show();
+                resetForm();
+            } catch (Exception e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Person Save Error\n" + e.getMessage());
+                alert.show();
+            }
+        });
+
+
+    }
+    private void resetForm() {
+        usernameTxt.clear();
+        passwordTxt.clear();
+        nameTxt.clear();
+        familyTxt.clear();
+        phoneNumberTxt.clear();
+        diseaseTxt.clear();
+        nationalIdTxt.clear();
+
 
     }
 }
---
+++ src/reception/model/utils/Validation.java
@@ -0,0 +1,44 @@
+package reception.model.utils;
+
+import java.util.regex.Pattern;
+
+public class Validation {
+
+    public  String NameValidator(String name) throws Exception {
+        if(Pattern.matches("^[a-zA-Z\\s]{2,30}$", name)){
+            return name;
+        }else{
+            throw new Exception("Invalid Name");
+        }
+    }
+
+    public  String familyValidator(String family) throws Exception {
+        if(Pattern.matches("^[a-zA-Z\\s]{2,30}$", family)){
+            return family;
+        }else{
+            throw new Exception("Invalid family");
+        }
+    }
+    public  String nationalIdValidator(String nationalId) throws Exception {
+        if(Pattern.matches("^[0-9]{10}$", nationalId)){
+            return nationalId;
+        }else{
+            throw new Exception("Invalid national ID");
+        }
+    }
+    public  String phoneNumberValidator(String phoneNumber) throws Exception {
+        if(Pattern.matches("^[0-9]{11}$", phoneNumber)){
+            return phoneNumber;
+        }else{
+            throw new Exception("Invalid phone number");
+        }
+    }
+
+    public  String diseaseValidator(String disease) throws Exception {
+        if(Pattern.matches("^[a-zA-Z\\s]{2,30}$", disease)){
+            return disease;
+        }else{
+            throw new Exception("Invalid disease");
+        }
+    }
+}
