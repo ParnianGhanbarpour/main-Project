9c7eefe62284369a671107e2c8d146ca99557cc7
fatemeh <fatemehsadat.h9@gmail.com>
2024-10-03 23:02:50 +0330

    WorkShift

--- src/reception/controllers/VisitTimeController.java
+++ src/reception/controllers/VisitTimeController.java
@@ -4,9 +4,12 @@ package reception.controllers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import reception.model.da.DoctorDa;
 import reception.model.da.VisitTimeDa;
 import reception.model.da.WorkShiftDa;
@@ -23,37 +26,44 @@ public class VisitTimeController implements Initializable {
 
     private final Validation validation = new Validation();
     @FXML
-    private TextField idTxt,shiftIdTxt,patientIdTxt,paymentIdTxt,roomNumberTxt,prescriptionIdTxt,durationTxt;
+    private TextField idTxt, shiftIdTxt, patientIdTxt, paymentIdTxt, roomNumberTxt, prescriptionIdTxt, durationTxt;
     @FXML
     private DatePicker visitDatePicker;
     @FXML
     private ComboBox<String> expertiseCmb;
     @FXML
-    private ComboBox<Integer> hourCmb , minutesCmb;
+    private ComboBox<Integer> hourCmb, minutesCmb;
     @FXML
-   private TableView visitTbl,
+    private TableView doctorTbl,
             shiftTbl;
     @FXML
-    private TableColumn doctorIdCol,nameCol,familyCol,skillCol,
-            shiftIdCol,shiftDoctorIdCol,shiftDateCol;
+    private TableColumn doctorIdCol, nameCol, familyCol, skillCol,
+            shiftIdCol, shiftDoctorIdCol, shiftDateCol;
     @FXML
-    private Button findExpertiseBtn,findDoctorBtn,findPatientBtn,findDateBtn;
+    private Button findExpertiseBtn, findDoctorBtn, findPatientBtn, findDateBtn;
     @FXML
     private Button saveBtn, editBtn, removeBtn;
+
+    @FXML
+    private TableView visitTimeTbl;
+    @FXML
+    private TableColumn idCol,workShiftIdCol,patientIdCol,paymentIdCol,
+            RomCol,prescriptionIdCol,visitDateCol,hourCol,minuteCol,durationCol;
 
     private Doctor doctor;
 
     private Set<String> selectedTimes = new HashSet<>();
-    private Set <String> selectedDays=new HashSet<>();
+    private Set<String> selectedDays = new HashSet<>();
 
     @Override
-    public void initialize(URL location, ResourceBundle resources ) {
+    public void initialize(URL location, ResourceBundle resources) {
 
         for (int i = 10; i <= 20; i++) {
-            hourCmb.getItems().add(i);}
+            hourCmb.getItems().add(i);
+        }
         for (int m = 0; m < 60; m += 30) {
-            minutesCmb.getItems().add(m) ;}
-
+            minutesCmb.getItems().add(m);
+        }
 
 
 //        configureAccess();
@@ -97,8 +107,6 @@ public class VisitTimeController implements Initializable {
             } catch (Exception e) {
                 Alert alert = new Alert(Alert.AlertType.ERROR, "VisitTime Save Error\n" + e.getMessage());
                 alert.show();
-
-
 
 
             }
@@ -156,6 +164,18 @@ public class VisitTimeController implements Initializable {
         });
 
 
+        findPatientBtn.setOnAction(event -> openVisitTimeTable("VisitTimeTbl.fxml" , "VisitTimeTable")); {
+            try (VisitTimeDa visitTimeDa=new VisitTimeDa()) {
+                refreshTable(visitTimeDa.findAll());
+            } catch (Exception e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, " Error3\n" + e.getMessage());
+                alert.show();
+            }
+
+
+        }
+
+
 
 
     }
@@ -209,92 +229,106 @@ public class VisitTimeController implements Initializable {
         */
 
 
-        private void resetForm () throws Exception {
-            idTxt.clear();
-            shiftIdTxt.clear();
-            patientIdTxt.clear();
-            paymentIdTxt.clear();
-            roomNumberTxt.clear();
-            prescriptionIdTxt.clear();
-            durationTxt.clear();
-            expertiseCmb.getSelectionModel().clearSelection();
+    private void resetForm() throws Exception {
+        idTxt.clear();
+        shiftIdTxt.clear();
+        patientIdTxt.clear();
+        paymentIdTxt.clear();
+        roomNumberTxt.clear();
+        prescriptionIdTxt.clear();
+        durationTxt.clear();
+        expertiseCmb.getSelectionModel().clearSelection();
 
-            try (DoctorDa doctorDa=new DoctorDa()) {
-                refreshTableV(doctorDa.findAll());
-            }
-            catch (Exception e) {
-                Alert alert = new Alert(Alert.AlertType.ERROR, " Error1\n" + e.getMessage());
-                alert.show();
-            }
-
-            try (WorkShiftDa workShiftDa=new WorkShiftDa()) {
-                refreshTableSh(workShiftDa.findAll());
-            }
-            catch (Exception e) {
-                Alert alert = new Alert(Alert.AlertType.ERROR, " Error2\n" + e.getMessage());
-                alert.show();
-            }
-
-
+        try (DoctorDa doctorDa = new DoctorDa()) {
+            refreshTableV(doctorDa.findAll());
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, " Error1\n" + e.getMessage());
+            alert.show();
         }
 
-       public void addTime(){
+        try (WorkShiftDa workShiftDa = new WorkShiftDa()) {
+            refreshTableSh(workShiftDa.findAll());
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, " Error2\n" + e.getMessage());
+            alert.show();
+        }
 
-            Integer hour = hourCmb.getValue();
-            Integer minute = minutesCmb.getValue();
-            LocalDate Day=visitDatePicker.getValue();
-            if (Day==null||hour == null || minute == null) {
-                throw new IllegalArgumentException("Please select Date and both hour and minute.\n");
-           }
 
-           String date =Day.toString();
-           String time = hour + ":" + (minute < 10 ? "0" + minute : minute);
-          if (selectedDays.contains(date)){
+    }
+
+    public void addTime() {
+
+        Integer hour = hourCmb.getValue();
+        Integer minute = minutesCmb.getValue();
+        LocalDate Day = visitDatePicker.getValue();
+        if (Day == null || hour == null || minute == null) {
+            throw new IllegalArgumentException("Please select Date and both hour and minute.\n");
+        }
+
+        String date = Day.toString();
+        String time = hour + ":" + (minute < 10 ? "0" + minute : minute);
+        if (selectedDays.contains(date)) {
             if (selectedTimes.contains(time)) {
                 throw new IllegalArgumentException("Error: This time is already selected, please choose another time.\n");
             }
-          }
-               else {
-              selectedDays.add(date);
-              selectedTimes.add(time);
-              Alert alert = new Alert(Alert.AlertType.INFORMATION, "Visit time at " + time + "on" + date + " has been successfully added.\n");
+        } else {
+            selectedDays.add(date);
+            selectedTimes.add(time);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Visit time at " + time + "on" + date + " has been successfully added.\n");
 
-              alert.show();
-          }
+            alert.show();
         }
-
-        private void refreshTableV(List <Doctor> doctorList) {
-            ObservableList<Doctor> doctors= FXCollections.observableList(doctorList);
-
-            doctorIdCol.setCellValueFactory(new PropertyValueFactory<>("doctorId"));
-            nameCol.setCellValueFactory(new PropertyValueFactory<>("Name"));
-            familyCol.setCellValueFactory(new PropertyValueFactory<>("family"));
-            skillCol.setCellValueFactory(new PropertyValueFactory<>("expertise"));
-
-            visitTbl.setItems(doctors);
-
-        }
-
-        private void refreshTableSh(List<WorkShift>workShiftList){
-            ObservableList<WorkShift> workShifts= FXCollections.observableList(workShiftList);
-
-            shiftIdCol.setCellValueFactory(new PropertyValueFactory<>("workShiftId"));
-            shiftDoctorIdCol.setCellValueFactory(new PropertyValueFactory<>("shiftDoctorId"));
-            shiftDateCol.setCellValueFactory(new PropertyValueFactory<>("shiftDate"));
-
-            shiftTbl.setItems(workShifts);
-        }
-
-/*    private void refreshTable(List<Person> personList) {
-        ObservableList<Person> persons = FXCollections.observableList(personList);
-
-        idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
-        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
-        familyCol.setCellValueFactory(new PropertyValueFactory<>("family"));
-        genderCol.setCellValueFactory(new PropertyValueFactory<>("gender"));
-
-        personTbl.setItems(persons);*/
     }
 
+    private void refreshTableV(List<Doctor> doctorList) {
+        ObservableList<Doctor> doctors = FXCollections.observableList(doctorList);
+
+        doctorIdCol.setCellValueFactory(new PropertyValueFactory<>("doctorId"));
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        familyCol.setCellValueFactory(new PropertyValueFactory<>("family"));
+        skillCol.setCellValueFactory(new PropertyValueFactory<>("expertise"));
+
+        doctorTbl.setItems(doctors);
+
+    }
+
+    private void refreshTableSh(List<WorkShift> workShiftList) {
+        ObservableList<WorkShift> workShifts = FXCollections.observableList(workShiftList);
+
+        shiftIdCol.setCellValueFactory(new PropertyValueFactory<>("workShiftId"));
+        shiftDoctorIdCol.setCellValueFactory(new PropertyValueFactory<>("shiftDoctorId"));
+        shiftDateCol.setCellValueFactory(new PropertyValueFactory<>("shiftDate"));
+
+        shiftTbl.setItems(workShifts);
+    }
+
+    private void refreshTable ( List <VisitTime> visitTimeList){
+        ObservableList<VisitTime> visitTimes = FXCollections.observableList(visitTimeList);
+
+                idCol.setCellValueFactory(new PropertyValueFactory<>("Id"));
+                workShiftIdCol.setCellValueFactory(new PropertyValueFactory<>("ShiftId"));
+                patientIdCol.setCellValueFactory(new PropertyValueFactory<>("PatientId"));
+                paymentIdCol.setCellValueFactory(new PropertyValueFactory<>("PaymentId"));
+                RomCol.setCellValueFactory(new PropertyValueFactory<>("Room"));
+                prescriptionIdCol.setCellValueFactory(new PropertyValueFactory<>("PrescriptionId"));
+                visitDateCol.setCellValueFactory(new PropertyValueFactory<>("VisitDate"));
+                hourCol.setCellValueFactory(new PropertyValueFactory<>("Hour"));
+                minuteCol.setCellValueFactory(new PropertyValueFactory<>("Minute"));
+                durationCol.setCellValueFactory(new PropertyValueFactory<>("Duration"));
+
+                visitTimeTbl.setItems(visitTimes);
+    }
+    private void openVisitTimeTable(String fxmlFile, String title) {
+        try {
+            Stage stage = new Stage();
+            Scene scene = new Scene(FXMLLoader. load(getClass().getResource("/reception/view/" + fxmlFile)));
+            stage.setScene(scene);
+            stage.setTitle(title);
+            stage.show();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
 
 
